4cad191f2c2cebbcf0bf32b5fdb0be745bf657b9
Mykhailo Shpilienko <m.shpilenko@gmail.com>
2023-03-24 03:44:46 +0200

    Extentreport implemented

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -17,8 +17,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(monochrome = true, features = "src/test/java/features", glue = "stepdefs", plugin = { "pretty",
-		"json:target/cucumber.json" }, tags = ("@sanity"))
+		"json:target/cucumber.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, tags = ("@smoke"))
 
+//publish =true
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 	public static WebDriver driver;
@@ -65,6 +66,6 @@ public class TestRunner extends AbstractTestNGCucumberTests {
 	public void TearDown() throws InterruptedException, ClassNotFoundException, SQLException {
 		cleanDB();
 		db.closeConnection();
-		driver.close();
+		//driver.close();
 	}
 }
