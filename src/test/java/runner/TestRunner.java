51fecf26a34a9b1c815b6e5e4576472136b3add2
Mykhailo Shpilienko <m.shpilenko@gmail.com>
2023-03-26 01:14:09 +0200

    TestRunner formatted

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -17,9 +17,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(monochrome = true, features = "src/test/java/features", glue = "stepdefs", plugin = { "pretty",
-		"json:target/cucumber.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, tags = ("@sanity"))
+		"json:target/cucumber.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, tags = ("@e2e or @regression"), publish=true)
 
-//publish =true
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 	public static WebDriver driver;
@@ -37,7 +36,6 @@ public class TestRunner extends AbstractTestNGCucumberTests {
 			System.out.println("TEST DATA CLEANUP COMPLETE");
 			System.out.println(StringUtils.repeat("=", 47) + "\n");
 		}
-
 	}
 
 	@BeforeSuite(alwaysRun = true)
