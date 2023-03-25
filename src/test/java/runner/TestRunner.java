fc38e1d2d7e3a7b1b7218f1d0f3865ab802e7b89
Mykhailo Shpilienko <m.shpilenko@gmail.com>
2023-03-25 16:49:08 +0200

    TestNG runner modified

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -12,12 +12,12 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
-import common.DbUtils;
+import db.DbUtils;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(monochrome = true, features = "src/test/java/features", glue = "stepdefs", plugin = { "pretty",
-		"json:target/cucumber.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, tags = ("@smoke"))
+		"json:target/cucumber.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, tags = ("@sanity"))
 
 //publish =true
 public class TestRunner extends AbstractTestNGCucumberTests {
@@ -66,6 +66,6 @@ public class TestRunner extends AbstractTestNGCucumberTests {
 	public void TearDown() throws InterruptedException, ClassNotFoundException, SQLException {
 		cleanDB();
 		db.closeConnection();
-		//driver.close();
+		driver.close();
 	}
 }
